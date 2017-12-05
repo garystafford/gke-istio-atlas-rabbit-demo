b1e16e1db97ad8209483d19c142f6ae58b694974
Gary Stafford <gary.stafford@accenture.com>
2017-12-05 10:20:40 -0500

    Refactor scripts, new images

---
+++ kubernetes/voter-api/drop_documents.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# clean out all documents from CosmosDB database collections
+
+urls=(
+  'https://api.voter-demo.com/election/drop/elections'
+  'https://api.voter-demo.com/candidate/drop/candidates'
+  'https://api.voter-demo.com/candidate/drop/elections'
+  'https://api.voter-demo.com/voter/drop/candidates'
+  'https://api.voter-demo.com/voter/drop/votes'
+)
+
+for url in "${urls[@]}"
+do
+  echo "POSTing to ${url}"
+  curl --request POST --url ${url}
+done
