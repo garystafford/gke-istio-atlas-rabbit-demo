510686597bc7a7dded227bd437214676f4406048
Gary Stafford <gary.stafford@accenture.com>
2017-12-15 01:08:06 -0500

    Adding Istio to Voter API

--- kubernetes/voter-api-atlas/create-voter-api_part2.bat
+++ kubernetes/voter-api-atlas/create-voter-api_part2.bat
@@ -2,22 +2,9 @@ REM apply Voter API Resources Part 2
 
 REM Services
 kubectl apply ^
-  -f .\services\candidate-deployment.yaml ^
-  -f .\services\candidate-service.yaml ^
-  -f .\services\election-deployment.yaml ^
   -f .\services\election-service.yaml ^
-  -f .\services\voter-deployment.yaml ^
+  -f .\services\candidate-service.yaml ^
   -f .\services\voter-service.yaml
-
-REM istioctl kube-inject -f .\services\candidate-deployment.yaml
-REM istioctl kube-inject -f .\services\election-deployment.yaml
-REM istioctl kube-inject -f  .\services\voter-deployment.yaml
-
-REM wait for services to come up fully
-timeout 120
-
-REM Ingress
-REM kubectl apply -f .\ingress.yaml
 
 REM Istio Ingress
 kubectl create -f .\ingress-istio.yaml
