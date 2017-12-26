66f1fea0a69ab3647cd0aab2ae78c0590976d6fd
Gary A. Stafford <garystafford@rochester.rr.com>
2017-12-26 11:24:33 -0500

    Refactor scripts

--- kubernetes/voter-api-atlas/create-voter-api-part3.sh
+++ kubernetes/voter-api-atlas/create-voter-api-part3.sh
@@ -4,12 +4,12 @@
 # manual sidecar injection with istioctl kube-inject
 # istio egress of mongodb and amqp protocols
 
-export IP_RANGES="10.12.0.0/14,10.15.240.0/20"
+IP_RANGES="10.12.0.0/14,10.15.240.0/20"
 
 # candidate service
 istioctl kube-inject –kubeconfig "~/.kube/config" \
   -f ./services/candidate-deployment.yaml \
-  --includeIPRanges=${IP_RANGES} > \
+  --includeIPRanges=$IP_RANGES > \
   candidate-deployment-istio.yaml \
   && kubectl apply -f candidate-deployment-istio.yaml \
   && rm candidate-deployment-istio.yaml
@@ -17,7 +17,7 @@ istioctl kube-inject –kubeconfig "~/.kube/config" \
 # election service
 istioctl kube-inject –kubeconfig "~/.kube/config" \
   -f ./services/election-deployment.yaml \
-  --includeIPRanges=${IP_RANGES} > \
+  --includeIPRanges=$IP_RANGES > \
   election-deployment-istio.yaml \
   && kubectl apply -f election-deployment-istio.yaml \
   && rm election-deployment-istio.yaml
@@ -25,7 +25,7 @@ istioctl kube-inject –kubeconfig "~/.kube/config" \
 # voter service
 istioctl kube-inject –kubeconfig "~/.kube/config" \
   -f ./services/voter-deployment.yaml \
-  --includeIPRanges=${IP_RANGES} > \
+  --includeIPRanges=$IP_RANGES > \
   voter-deployment-istio.yaml \
   && kubectl apply -f voter-deployment-istio.yaml \
   && rm voter-deployment-istio.yaml
