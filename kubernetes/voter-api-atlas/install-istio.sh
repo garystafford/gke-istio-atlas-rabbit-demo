5b945c7e76d4ffebf3fac1a947f4b00ad2e71ad8
Gary A. Stafford <garystafford@rochester.rr.com>
2017-12-26 11:24:45 -0500

    Refactor scripts

--- kubernetes/voter-api-atlas/install-istio.sh
+++ kubernetes/voter-api-atlas/install-istio.sh
@@ -3,10 +3,9 @@
 # install istio, add-ons, and roles
 # https://cloud.google.com/kubernetes-engine/docs/tutorials/istio-on-gke
 
-export ISTIO_HOME="/Applications/istio-0.4.0"
+ISTIO_HOME="/Applications/istio-0.4.0"
 
-# required dashboard and istio roles
-# Error from server (Forbidden): error when creating ".../istio-auth.yaml":
+# required dashboard access and istio roles
 kubectl apply \
   -f ./other/kube-system-cluster-admin.yaml \
   -f ./other/cluster-admin-binding.yaml
@@ -14,16 +13,16 @@ kubectl apply \
 
 # istio
 kubectl apply \
-  -f ${ISTIO_HOME}/install/kubernetes/istio-auth.yaml \
-  -f ${ISTIO_HOME}/install/kubernetes/istio-initializer.yaml
+  -f $ISTIO_HOME/install/kubernetes/istio-auth.yaml \
+  -f $ISTIO_HOME/install/kubernetes/istio-initializer.yaml
 
 # add-ons
 kubectl apply \
-  -f ${ISTIO_HOME}/install/kubernetes/addons/prometheus.yaml \
-  -f ${ISTIO_HOME}/install/kubernetes/addons/grafana.yaml \
-  -f ${ISTIO_HOME}/install/kubernetes/addons/servicegraph.yaml \
-  -f ${ISTIO_HOME}/install/kubernetes/addons/zipkin.yaml \
-  -f ${ISTIO_HOME}/install/kubernetes/addons/zipkin-to-stackdriver.yaml
+  -f $ISTIO_HOME/install/kubernetes/addons/prometheus.yaml \
+  -f $ISTIO_HOME/install/kubernetes/addons/grafana.yaml \
+  -f $ISTIO_HOME/install/kubernetes/addons/servicegraph.yaml \
+  -f $ISTIO_HOME/install/kubernetes/addons/zipkin.yaml \
+  -f $ISTIO_HOME/install/kubernetes/addons/zipkin-to-stackdriver.yaml
 
 # alternate zipkin-to-stackdriver, configured fully
   # -f ./other/zipkin-to-stackdriver.yaml
